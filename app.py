1e33a8970b1fdfd3aa8b737cd0989f0a17d49654
ahibrahimgad-tech <ahibrahimgad@gmail.com>
2026-03-12 10:33:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ with tab1:
     col1, col2 = st.columns(2)
     with col1:
         uld_no = st.text_input("ULD No")
-        airline = st.selectbox("Airline", ["RMX", "SVI", "BBT", "MNG", "SH","Avairy", "Air Challenge"])
+        airline = st.selectbox("Airline", ["RMX", "SVI", "BBT", "MNG", "SH","Avairy","National Air","Air Challenge"])
         flight_no = st.text_input("Arrival Flight No")
     
     with col2:
@@ -273,6 +273,7 @@ footer = """
 </div>
 """
 st.markdown(footer, unsafe_allow_html=True)
+
 
 
 
